26dfaf89ee200c64d558311657665d2efa4e557c
Umindu Nimsara <umindunimz@gmail.com>
2025-11-20 20:08:23 +0530

    Version 1

---
+++ helpers/math_helper.py
@@ -0,0 +1,5 @@
+def multiply(a, b):
+    return a * b
+
+if __name__ == '__main__':
+    print("Product:", multiply(4, 5))
---
+++ helpers/string_helper.py
@@ -0,0 +1,5 @@
+def shout(text):
+    return text.upper()
+
+if __name__ == "__main__":
+    print(shout("hello"))
